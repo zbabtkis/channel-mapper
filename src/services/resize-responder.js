0ab02026c60d363f4d5a4f86edd31a948e2cb100
zigzackattack <zackbabtkis@gmail.com>
2014-02-07 11:55:12 -0800

    debounced resize service callbacks, changed remove method so that it uses splice instead of delete to remove callbacks without creating undefined array indexes

--- src/services/resize-responder.js
+++ src/services/resize-responder.js
@@ -1,22 +1,47 @@
-app.service('resizeResponder', function($rootScope) {
+app.service('resizeResponder', function($rootScope, $window) {
 	"use strict";
 
-	var callbacks = [];
+	var callbacks = []
+	  , bounceLimit = 200;
 
-	window.onresize = function() {
+	function debounce(func) {
+		var timeout;
+
+		return function() {
+			if(timeout) return;
+			timeout = setTimeout(function() {
+				func.apply(this, arguments);
+				clearTimeout(timeout); timeout = undefined;
+			}, bounceLimit);
+		};
+	};
+
+	function loopResponders() {
+		var args = arguments;
+
 		callbacks.forEach(function(func) {
-			func();
+			if(!angular.isFunction(func)) return;
+
+			func.apply($window, args);
 			$rootScope.$apply();
 		});
 	};
 
+	/** Public Service API */
+
+	// Add callback to window resize event.
 	this.respond = function(func) {
-		callbacks.push(func);
-
-		return callbacks.length - 1;
+		return angular.isFunction(func) && 
+			callbacks.push(func) &&
+			func;
 	};
 
-	this.remove = function(id) {
-		delete callbacks[id];
+	// Remove callback from window resize event.
+	this.remove = function(func) {
+		return angular.isFunction(func) && 
+			callbacks.splice(callbacks.indexOf(func), 1) &&
+			func;
 	};
+
+	$window.addEventListener('resize', debounce(loopResponders));
 });
